b22f6807d01b0860620292f9351168be716d9841
RuthXD <131949256+RuthXD@users.noreply.github.com>
2023-04-27 09:24:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onGesture(Gesture.TiltRight, function () {
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
 	
 })
